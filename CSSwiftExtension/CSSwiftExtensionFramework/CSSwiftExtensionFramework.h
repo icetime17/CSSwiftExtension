9d6410033146a5a6c74b85922ebbb52543f53fc9
Chris Hu <icetime17@126.com>
2016-12-30 17:24:55 +0800

    Add Carghage

---
+++ CSSwiftExtension/CSSwiftExtensionFramework/CSSwiftExtensionFramework.h
@@ -0,0 +1,19 @@
+//
+//  CSSwiftExtensionFramework.h
+//  CSSwiftExtensionFramework
+//
+//  Created by Chris Hu on 16/12/30.
+//  Copyright © 2016年 com.icetime17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSSwiftExtensionFramework.
+FOUNDATION_EXPORT double CSSwiftExtensionFrameworkVersionNumber;
+
+//! Project version string for CSSwiftExtensionFramework.
+FOUNDATION_EXPORT const unsigned char CSSwiftExtensionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSwiftExtensionFramework/PublicHeader.h>
+
+
